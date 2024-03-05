c8c244ce6ce030e4d49c0e9980940335dbea7bf1
SonicX-svg <143742185+SonicX-svg@users.noreply.github.com>
2024-03-05 04:23:14 +0300

    Add files via upload

--- test_src.py
+++ test_src.py
@@ -1,8 +1,11 @@
-from  scr1 import summa
-#from src import pc
+from src.scr1 import summa
+
+
+# from src import pc
 def test_func():
-  assert summa(1,2)==3
-  
+    assert summa(1, 2) == 3
+
+
 if __name__ == "__main__":
     test_func()
     print("Everything passed")
